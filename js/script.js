f233d62573fd08ef5f45a58bb31aa92d9b834de3
Dias Utsman <utsmand91@gmail.com>
2022-08-29 15:03:54 +0700

    Make menu closable outside burger menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 const menuBurger = document.querySelector('.menu-toggle')
 const nav = document.querySelector('nav ul')
 
-menuBurger.addEventListener('click', () => {
+menuBurger.addEventListener('click', e => {
     nav.classList.toggle('slide')
+    e.stopPropagation()
+})
+
+nav.querySelectorAll('a').forEach(e => e.addEventListener('click', e => {
+    menuBurger.firstElementChild.click()
+    e.stopPropagation()
+}))
+
+document.body.addEventListener('click', e => {
+    menuBurger.firstElementChild.click()
 })
