ebe829e2aff9bc50559aa816981af893898fe2f1
Dias Utsman <utsmand91@gmail.com>
2022-08-30 06:38:11 +0700

    Make menu closable when clicked outside it

--- js/script.js
+++ js/script.js
@@ -3,14 +3,19 @@ const nav = document.querySelector('nav ul')
 
 menuBurger.addEventListener('click', e => {
     nav.classList.toggle('slide')
+})
+
+// Make menu closed when one of links is clicked
+nav.querySelectorAll('a').forEach(e => e.addEventListener('click', e => {
+    if(nav.classList.contains('slide'))menuBurger.firstElementChild.click()
+}))
+
+// Make body cannot be triggered when click is inside nav
+nav.parentElement.addEventListener('click', e => {
     e.stopPropagation()
 })
 
-nav.querySelectorAll('a').forEach(e => e.addEventListener('click', e => {
-    menuBurger.firstElementChild.click()
-    e.stopPropagation()
-}))
-
+// close menu when 
 document.body.addEventListener('click', e => {
     menuBurger.firstElementChild.click()
 })
