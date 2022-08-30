8465ec31d85d1aea334a615d03f20cb6d5d21bba
Dias Utsman <utsmand91@gmail.com>
2022-08-30 20:25:05 +0700

    Add popular class feature

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,10 @@
-String.prototype.toCamelCase = function() {
+String.prototype.toCamelCase = function () {
     return this.replace(/[-_]\w/g, e => e.slice(-1).toUpperCase())
+}
+
+Number.prototype.toRupiahFormat = function () {
+    return this.toLocaleString('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+    })
 }
