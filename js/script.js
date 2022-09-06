f37cbca328c225bad11920102ec1b273690a424c
Dias Utsman <utsmand91@gmail.com>
2022-09-06 14:53:49 +0700

    Some change in animation

--- js/script.js
+++ js/script.js
@@ -94,8 +94,6 @@ function scrollReveal() {
 
             if (elementTop < windowHeight - elementVisible) {
                 reveals[i].classList.add("active");
-            } else {
-                reveals[i].classList.remove("active");
             }
         }
     })
