4f9d85fd00bfe5d86a562d3145851105b93ffba2
Dias Utsman <utsmand91@gmail.com>
2022-09-09 14:26:07 +0700

    Change some sliders features

--- js/script.js
+++ js/script.js
@@ -153,7 +153,7 @@ function testimonialsSlider() {
         dots[slideIndex - 1].className += " active-slider";
     }
 
-    setInterval(() => showSlides(++slideIndex), 3000)
+    setInterval(() => showSlides(++slideIndex), 5000)
 }
 
 try {
